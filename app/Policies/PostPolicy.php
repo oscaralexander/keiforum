0457b349c2356b8dfd7e47ff8768d0d145ec3082
Alexander Griffioen <alex.griffioen@gmail.com>
2026-03-10 07:40:26 +0100

    Remove strict === comparison on IDs

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -14,11 +14,11 @@ class PostPolicy
 
     public function update(User $user, Post $post): bool
     {
-        return $user->id === $post->user_id || $user->is_admin;
+        return $user->id == $post->user_id || $user->is_admin;
     }
 
     public function delete(User $user, Post $post): bool
     {
-        return $user->id === $post->user_id || $user->is_admin;
+        return $user->id == $post->user_id || $user->is_admin;
     }
 }
