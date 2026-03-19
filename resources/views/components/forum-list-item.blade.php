3a62ce9f571a794d3dcf77ff6ff1a6e4b744e56e
Alexander Griffioen <alex.griffioen@gmail.com>
2026-03-19 21:14:59 +0100

    Styling

--- resources/views/components/forum-list-item.blade.php
+++ resources/views/components/forum-list-item.blade.php
@@ -8,46 +8,48 @@
 ])
 
 <li class="forumListItem">
-    <div class="forumListItem__icon">
-        <x-icon icon="{{ $forum->icon }}" />
-    </div>
-    <div class="forumListItem__text">
+    <header class="forumListItem__header">
+        <div class="forumListItem__icon">
+            <x-icon icon="{{ $forum->icon }}" />
+        </div>
         <div class="forumListItem__nameDescription">
             <a class="forumListItem__name" href="{{ route('forum.show', $forum->slug) }}" wire:navigate>{{ $forum->name }}</a>
             <div class="forumListItem__description">{{ $forum->description }}</div>
         </div>
-        @if ($forum->recentTopics->isNotEmpty())
-            <ul class="forumListItem__recentTopics">
-                @foreach ($forum->recentTopics as $topic)
-                    <li class="forumListItem__recentTopic" wire:key="recent-topic-{{ $topic->id }}">
+    </header>
+    @if ($forum->recentTopics->isNotEmpty())
+        <ul class="forumListItem__recentTopics">
+            @foreach ($forum->recentTopics as $topic)
+                <li class="forumListItem__recentTopic" wire:key="recent-topic-{{ $topic->id }}">
+                    <div class="forumListItem__recentTopic-icon">
                         <x-avatar class="m:hide" :size="AvatarSize::XS" :user="$topic->latestPost->user" />
                         <x-avatar class="m:show" :size="AvatarSize::S" :user="$topic->latestPost->user" />
-                        <div>
-                            <a class="forumListItem__recentTopic-title" href="{{ route('topic.show', [$forum, $topic, $topic->slug]) }}" wire:navigate>{{ $topic->title }}</a>
-                            <ul class="meta">
-                                <li class="meta__item">
-                                    <x-icon icon="message-circle" />
-                                    {{ $topic->posts_count }}
-                                </li>
-                                <li class="meta__item">
-                                    <a href="{{ route('topic.show', [$topic->forum, $topic, $topic->slug, 'post' => $topic->latestPost->id]) }}" wire:navigate>
-                                        {{ time_diff($topic->latestPost?->created_at) }} @lang('ui.ago')
-                                        <x-icon icon="arrow-right" />
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                @endforeach
-            </ul>
-            @php
-                $moreTopicsCount = $forum->topics_count - 5;
-            @endphp
-            @if ($moreTopicsCount > 0)
-                <a class="forumListItem__moreTopics" href="{{ route('forum.show', $forum->slug) }}" wire:navigate>
-                    {{ trans_choice('forum/index.more_topics', $moreTopicsCount, ['count' => $moreTopicsCount]) }}
-                </a>
-            @endif
+                    </div>
+                    <div>
+                        <a class="forumListItem__recentTopic-title" href="{{ route('topic.show', [$forum, $topic, $topic->slug]) }}" wire:navigate>{{ $topic->title }}</a>
+                        <ul class="meta">
+                            <li class="meta__item">
+                                <x-icon icon="message-circle" />
+                                {{ $topic->posts_count }}
+                            </li>
+                            <li class="meta__item">
+                                <a href="{{ route('topic.show', [$topic->forum, $topic, $topic->slug, 'post' => $topic->latestPost->id]) }}" wire:navigate>
+                                    {{ time_diff($topic->latestPost?->created_at) }} @lang('ui.ago')
+                                    <x-icon icon="arrow-right" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+        @php
+            $moreTopicsCount = $forum->topics_count - 5;
+        @endphp
+        @if ($moreTopicsCount > 0)
+            <a class="forumListItem__moreTopics" href="{{ route('forum.show', $forum->slug) }}" wire:navigate>
+                {{ trans_choice('forum/index.more_topics', $moreTopicsCount, ['count' => $moreTopicsCount]) }}
+            </a>
         @endif
-    </div>
+    @endif
 </li>
